630a518311d10c4d1049307903ec1e8389d7cc09
vishnuvp <vishnuvp@ymail.com>
2015-06-05 10:40:57 +0530

    version 1

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  
+  $("#name-hider").show().animate({width: "0%"},2500);
+  $("#my-name").fadeOut('slow').fadeIn('slow');
+
+  $(window).scroll(function(){
+   // $("#sticky-name").text(($(this).scrollTop()));
+    if(($(this).scrollTop()) > 390) 
+      $("#sticky-name,#up-nav").fadeIn();
+    else
+      $("#sticky-name,#up-nav").fadeOut();
+  });
+
+  jQuery('a.menu-link').click(function(e){
+    e.preventDefault();
+    var href= jQuery(this).attr("to");
+    jQuery('html, body').animate({
+      scrollTop: jQuery(href).offset().top - 150}, 1000); }); 
+
+jQuery('.inline-icon').mouseenter(function(){
+   $('.layer-1',this).animate({transform: 'translate(-60px, -60px) rotate(-45deg)'});
+ });
+  jQuery('.inline-icon').mouseleave(function(){
+   $('.layer-1',this).animate({transform: 'translate(0px, 0px) rotate(0deg)'});
+ });
+
+  $('.school-name,.org-name').mouseover(function() {
+    $('.display-pic',this).fadeIn('slow');
+  }).mouseleave(function () { $('.display-pic',this).fadeOut(); });
+
+  //$('#welcome-overlay').mouseenter(function() { $('#dp-f').fadeIn();}).mouseleave(function() { $('#dp-f').fadeOut();});
+});
