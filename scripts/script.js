b44aeabd2b0fa25cc504548b73caa57d14d18040
vishnuvp <devinbox.vishnu@gmail.com>
2016-05-13 02:44:18 +0530

    Blog feed on homepage

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+var blogs = "";
 $(document).ready(function(){
   
   $("#name-hider").show().animate({width: "0%"},2500);
@@ -30,10 +31,77 @@ jQuery('.inline-icon').mouseenter(function(){
 
   //$('#welcome-overlay').mouseenter(function() { $('#dp-f').fadeIn();}).mouseleave(function() { $('#dp-f').fadeOut();});
 
-  $(document).ready(function(){
+ // $(document).ready(function(){
   $("#icon-0").click(function(){
     $(".icon").toggleClass("no-translate")
   });
+  readFeed();
+  var pageSize = 3;
+//});
+function readFeed() {
+    
+    $.ajax({
+        type: 'GET',
+        url: "http://blog.vishnuvp.xyz/feeds/posts/summary?alt=json-in-script",
+        async: false,
+        contentType: "application/json",
+        dataType: 'jsonp',
+        success: function (json) {
+            //localStorage.setItem("blogList", json.feed.entry);
+            blogs = json.feed.entry;
+            var numberOfPages = Math.ceil(blogs.length/pageSize);
+            var line = "";
+            for (var i = 1; i <= numberOfPages; i++) {
+              line += "<a href='#feed-container' data='"+i+"' class='blog-pages'>"+i+"</a>"
+            };
+            $("#page-nos").html(line);
+            $(".blog-pages").first().addClass('clicked');
+            renderBlogList(blogs.slice(0,pageSize));  
+          }
+
+    });
+}
+
+$(document.body).on('click','.blog-pages',function (e) {
+      //e.preventDefault();
+      $('.blog-pages').removeClass('clicked');
+      $(this).addClass('clicked');
+      var start = ($(this).attr("data")-1)*pageSize;
+      renderBlogList(blogs.slice(start,start+pageSize));
+  });
+function renderBlogList(data) {
+    $('#feed-container ul').hide().html("").fadeIn();
+    var monthNames = [
+              "January", "February", "March",
+              "April", "May", "June", "July",
+              "August", "September", "October",
+              "November", "December"
+            ], days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    for (var i = 0; i < data.length; i++) {
+          var post = data[i],
+              title = post.title.$t,
+              summary = post.summary.$t,
+              date = post.published.$t;
+
+              date = new Date(date);
+              date = days[date.getDay()] +", "+date.getDate() +" "+ monthNames[date.getMonth()] +" "+ date.getFullYear();                
+              if (summary.length > 128) {
+                summary = summary.substr(0,128) + '...';
+              }
+              if (title.length > 64 ) {
+                title = title.substr(0,64) + '...';
+              };
+              $('#feed-container ul').append('<li><div class="blog-title">'+title+'</div><div class="blog-date">'+date+'</div><div class="blog-summary">'+summary+'</div><div class="blog-link"><a href="'+post.link[4].href+'" target="_blank">Read this article</div></li>');
+          
+        }
+}
+
+
 });
-  
-});
+
+
+
+
+
+
